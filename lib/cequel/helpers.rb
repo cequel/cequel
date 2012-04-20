2da4fdbbd2a5647c01cd08d3186018dba1ff8168
Mat Brown <mat.a.brown@gmail.com>
2012-04-20 15:47:32 -0400

    Serialize UUIDs as GUID strings

--- lib/cequel/helpers.rb
+++ lib/cequel/helpers.rb
@@ -5,6 +5,7 @@ module Cequel
     private
 
     def sanitize(statement, *bind_vars)
+      bind_vars.map! { |var| SimpleUUID::UUID === var ? var.to_guid : var }
       CassandraCQL::Statement.sanitize(statement, bind_vars)
     end
 
