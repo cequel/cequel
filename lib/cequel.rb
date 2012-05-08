57a87d0c85fbb233bb6631151cbfb9ea9f75cd6b
Mat Brown <mat.a.brown@gmail.com>
2012-05-08 14:01:55 -0400

    Add thrift client options when setting up connection

--- lib/cequel.rb
+++ lib/cequel.rb
@@ -12,10 +12,12 @@ require 'cequel/row_specification'
 
 module Cequel
   def self.connect(configuration)
+    thrift_options = configuration[:thrift] || {}
     Keyspace.new(
       CassandraCQL::Database.new(
         configuration[:host] || configuration[:hosts],
-        :keyspace => configuration[:keyspace]
+        {:keyspace => configuration[:keyspace]},
+        thrift_options.symbolize_keys
       )
     )
   end
