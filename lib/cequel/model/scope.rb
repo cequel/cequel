a01d95c935e5a1a4f15440c2fe5ec5abb1a71f03
Mat Brown <mat.a.brown@gmail.com>
2012-08-03 09:09:31 -0400

    find_in_batches accounts for duplicate last row/first row
    From what I've seen, Cassandra 1.0.10 will treat the > operator as >= if
    you also have a secondary index restriction.

--- lib/cequel/model/scope.rb
+++ lib/cequel/model/scope.rb
@@ -284,11 +284,18 @@ module Cequel
         end
 
         batch_scope = scope
+        last_key = nil
         begin
           batch_rows = batch_scope.to_a
-          yield batch_rows
+          break if batch_rows.empty?
+          if batch_rows.first[key_alias] == last_key
+            yield batch_rows[1..-1]
+          else
+            yield batch_rows
+          end
+          last_key = batch_rows.last[key_alias]
           batch_scope =
-            scope.where("? > ?", key_alias, batch_rows.last[key_alias])
+            scope.where("? > ?", key_alias, last_key)
         end while batch_rows.length == batch_size
       end
 
