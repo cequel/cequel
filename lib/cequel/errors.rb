ccc7309e86a6d4fdcfd72d3e2d7103d4c877856a
Mat Brown <mat.a.brown@gmail.com>
2013-11-29 16:20:10 -0500

    Documentation for errors

--- lib/cequel/errors.rb
+++ lib/cequel/errors.rb
@@ -1,5 +1,23 @@
 module Cequel
+  #
+  # @abstract
+  # @since 1.0.0
+  # Base class for all errors raised by Cequel
+  #
   Error = Class.new(StandardError)
+  #
+  # @since 1.0.0
+  #
+  # Raised when the schema defined in Cassandra cannot be modified to match
+  # the schema defined in the application (e.g., changing the type of a primary
+  # key)
+  #
   InvalidSchemaMigration = Class.new(Error)
+  #
+  # @since 1.0.0
+  #
+  # Raised when attempting to persist a Cequel::Record without defining all
+  # primary key columns
+  #
   MissingKeyError = Class.new(Error)
 end
