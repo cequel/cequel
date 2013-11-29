ea98e9bac79fad09a8270bc4341138fd67abf2b3
Mat Brown <mat.a.brown@gmail.com>
2013-11-29 16:36:45 -0500

    Documentation for CqlRowSpecification

--- lib/cequel/metal/cql_row_specification.rb
+++ lib/cequel/metal/cql_row_specification.rb
@@ -7,14 +7,30 @@ module Cequel
     #
     class CqlRowSpecification
 
+      #
+      # Build a new row specification
+      #
+      # @param (see #initialize)
+      # @return [Array<CqlRowSpecification>]
+      #
       def self.build(condition, bind_vars)
         [new(condition, bind_vars)]
       end
 
+      #
+      # Create a new row specification
+      #
+      # @param [String] condition CQL string representing condition
+      # @param [Array] bind_vars Bind variables
+      #
       def initialize(condition, bind_vars)
         @condition, @bind_vars = condition, bind_vars
       end
 
+      #
+      # CQL and bind variables for this condition
+      #
+      # @return [Array] CQL string followed by zero or more bind variables
       def cql
         [@condition, *@bind_vars]
       end
