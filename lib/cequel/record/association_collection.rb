3d2f93df22fff8e0d192ecc3e564a1f52eb0caa0
Mat Brown <mat.a.brown@gmail.com>
2013-11-30 21:51:31 -0500

    Documentation for AssociationCollection

--- lib/cequel/record/association_collection.rb
+++ lib/cequel/record/association_collection.rb
@@ -1,11 +1,20 @@
 module Cequel
-
   module Record
-
+    #
+    # Collection of records from a {Associations::ClassMethods#has_many has_many}
+    # associaiton. Encapsulates and behaves like a {RecordSet}, but unlike a
+    # normal RecordSet the loaded records are held in memory after they are
+    # loaded.
+    #
+    # @since 1.0.0
+    #
     class AssociationCollection < DelegateClass(RecordSet)
-
       include Enumerable
 
+      #
+      # @yield [Record]
+      # @return [void]
+      #
       def each(&block)
         target.each(&block)
       end
@@ -15,9 +24,6 @@ module Cequel
       def target
         @target ||= __getobj__.entries
       end
-
     end
-
   end
-
 end
