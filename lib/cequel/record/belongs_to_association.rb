22384209110087624c074be9ba9921bd8d888635
Mat Brown <mat.a.brown@gmail.com>
2013-11-30 22:36:55 -0500

    Documentation for BelongsToAssociation

--- lib/cequel/record/belongs_to_association.rb
+++ lib/cequel/record/belongs_to_association.rb
@@ -1,31 +1,56 @@
 module Cequel
-
   module Record
-
+    #
+    # Represents a parent association declared by
+    # {Associations::ClassMethods#belongs_to belongs_to}
+    #
+    # @see Associations::ClassMethods#parent_association
+    # @since 1.0.0
+    #
     class BelongsToAssociation
-
       extend Forwardable
 
-      attr_reader :owner_class, :name, :association_class_name
+      # @return [Class] child class that declared `belongs_to`
+      attr_reader :owner_class
+      # @return [Symbol] name of the association
+      attr_reader :name
+      # @return [String] name of parent class
+      attr_reader :association_class_name
 
+      # @!attribute [r] association_key_columns
+      #   @return [Array<Schema::Column>] key columns on the parent class
       def_delegator :association_class, :key_columns, :association_key_columns
 
+      #
+      # @param owner_class [Class] child class that declared `belongs_to`
+      # @param name [Symbol] name of the association
+      # @param options [Options] options for association
+      # @option options [String] :class_name name of parent class
+      # @option options [Symbol] :dependent (nil) whether to cascade deletions.
+      #   Valid values are :destroy, :delete
+      #
       def initialize(owner_class, name, options = {})
         @owner_class, @name = owner_class, name.to_sym
         @association_class_name =
           options.fetch(:class_name, @name.to_s.classify)
       end
 
+      #
+      # @return [Class] parent class declared by `belongs_to`
+      #
       def association_class
         @association_class ||= association_class_name.constantize
       end
 
+      #
+      # @return [Symbol] instance variable name to use for storing the parent
+      #   instance in a record
+      #
+      # @api private
+      #
       def instance_variable_name
         @instance_variable_name ||= :"@#{name}"
       end
-
     end
-
   end
-
 end
