06cbd8170ee16ef647d95c4ef2e6dccba6a2ba77
Mat Brown <mat.a.brown@gmail.com>
2013-12-22 18:03:19 -0500

    Documentation for Record::Schema

--- lib/cequel/record/schema.rb
+++ lib/cequel/record/schema.rb
@@ -1,9 +1,17 @@
 module Cequel
-
   module Record
-
+    #
+    # `Cequel::Record` implementations define their own schema in their class
+    # definitions. As well as defining attributes on record instances, the
+    # column definitions in {Properties} allow a `Cequel::Record` to have a
+    # precise internal represntation of its representation as a CQL3 table
+    # schema. Further, it is able to check this representation against the
+    # actual table defined in Cassandra (if any), and create or modify the
+    # schema in Cassandra to match what's defined in code.
+    #
+    # All the interesting stuff is in the {ClassMethods}.
+    #
     module Schema
-
       extend ActiveSupport::Concern
       extend Forwardable
 
@@ -12,22 +20,74 @@ module Cequel
         self.table_name = name.tableize.to_sym unless name.nil?
       end
 
+      #
+      # Methods available on {Record} class singletons to introspect and modify
+      # the schema defined in the database
+      #
       module ClassMethods
+        #
+        # @!attr table_name
+        #   @return [Symbol] name of the CQL table that backs this record class
+        #
+
         extend Forwardable
 
+        #
+        # @!attribute [r] columns
+        #   (see Schema::Table#columns)
+        #
+        # @!attribute [r] key_columns
+        #   (see Schema::Table#key_columns)
+        #
+        # @!attribute [r] key_column_names
+        #   (see Schema::Table#key_column_names)
+        #
+        # @!attribute [r] partition_key_columns
+        #   (see Schema::Table#partition_key_columns)
+        #
+        # @!attribute [r] clustering_columns
+        #   (see Schema::Table#clustering_columns)
+        #
+        # @!method compact_storage?
+        #   (see Schema::Table#compact_storage?)
+        #
         def_delegators :table_schema, :columns, :key_columns, :key_column_names,
           :partition_key_columns, :clustering_columns, :compact_storage?
+        #
+        # @!method reflect_on_column(column_name)
+        #   (see Schema::Table#column)
+        #
         def_delegator :table_schema, :column, :reflect_on_column
 
+        #
+        # Read the current schema assigned to this record's table from
+        # Cassandra, and make any necessary modifications (including creating
+        # the table for the first time) so that it matches the schema defined in
+        # the record definition
+        #
+        # @raise (see Schema::TableSynchronizer.apply)
+        # @return [void]
+        #
         def synchronize_schema
           Cequel::Schema::TableSynchronizer.
             apply(connection, read_schema, table_schema)
         end
 
+        #
+        # Read the current state of this record's table in Cassandra from the
+        # database.
+        #
+        # @return [Schema::Table] the current schema assigned to this record's
+        #   table in the database
+        #
         def read_schema
           connection.schema.read_table(table_name)
         end
 
+        #
+        # @return [Schema::Table] the schema as defined by the columns specified
+        #   in the class definition
+        #
         def table_schema
           @table_schema ||= Cequel::Schema::Table.new(table_name)
         end
@@ -76,9 +136,6 @@ module Cequel
       protected
       def_delegator 'self.class', :table_schema
       protected :table_schema
-
     end
-
   end
-
 end
