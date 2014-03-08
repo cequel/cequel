5c709352aff944a82bbbaf943baf0e00a5a43064
Mat Brown <mat.a.brown@gmail.com>
2014-03-07 20:04:16 -0500

    Cequel::uuid generates UUIDs from more types of input

---
+++ lib/cequel/uuids.rb
@@ -0,0 +1,43 @@
+# -*- encoding : utf-8 -*-
+module Cequel
+  #
+  # This module adds some utility methods for generating and type-checking UUID
+  # objects for use with Cequel. These methods are provided because the actual
+  # UUID implementation is an artifact of the underlying driver;
+  # initializing/typechecking those driver classes directly is potentially
+  # breaking.
+  #
+  module Uuids
+    #
+    # Create a UUID
+    #
+    # @param value [Time,String,Integer] timestamp to assign to the UUID, or
+    #   numeric or string representation of the UUID
+    # @return a UUID appropriate for use with Cequel
+    #
+    def uuid(value = nil)
+      if value.nil?
+        timeuuid_generator.next
+      elsif value.is_a?(Time) || value.is_a?(DateTime)
+        timeuuid_generator.from_time(value)
+      else Type::Timeuuid.instance.cast(value)
+      end
+    end
+
+    #
+    # Determine if an object is a UUID
+    #
+    # @param object an object to check
+    # @return [Boolean] true if the object is recognized by Cequel as a UUID
+    #
+    def uuid?(object)
+      object.is_a?(Cql::Uuid)
+    end
+
+    private
+
+    def timeuuid_generator
+      @timeuuid_generator ||= Cql::TimeUuid::Generator.new
+    end
+  end
+end
