74d1f5d2d3d93c9dd3f6a974679ea874d7b3966c
Mat Brown <mat.a.brown@gmail.com>
2014-03-16 10:42:53 -0400

    Simplified logging
    * Each Keyspace instance has one (optional) logger that is used for everything
    * Regular queries are logged at `debug` level; slow queries are logged at
      `warn` level; and queries that generate errors are logged at `error` level
    * Logger needs only implement `debug`, `warn`, and `error` methods that conform
      to the standard library logger's interface (so Log4r works)

--- lib/cequel/metal/logging.rb
+++ lib/cequel/metal/logging.rb
@@ -5,44 +5,27 @@ module Cequel
     # Methods to handle logging for {Keyspace} instances
     #
     module Logging
-      def logger=(logger)
-        loggers << Logger.new(logger, ::Logger::DEBUG)
-        self.exception_logger = ExceptionLogger.new(logger, ::Logger::ERROR)
-      end
+      extend Forwardable
+      def_delegators :request_logger, :logger, :logger=, :slowlog_threshold,
+                     :slowlog_threshold=
 
+      #
+      # @deprecated
+      #
       def slowlog=(slowlog)
-        warn "#slowlog= is deprecated and will be removed from a future " \
-             "version"
-        loggers << @slowlog = Logger.new(slowlog, ::Logger::WARN, 2000)
-      end
-
-      def slowlog_threshold=(threshold)
-        @slowlog.threshold = threshold
+        warn "#slowlog= is deprecated and ignored"
       end
 
       protected
 
-      attr_accessor :exception_logger
+      attr_writer :request_logger
 
-      private
-
-      def log(label, statement, *bind_vars)
-        response = nil
-        begin
-          time = Benchmark.ms { response = yield }
-          loggers.each do |logger|
-            logger.log(label, time, statement, bind_vars)
-          end
-        rescue Exception => e
-          exception_logger.log(label, statement, bind_vars) if exception_logger
-          raise
-        end
-        response
+      def request_logger
+        @request_logger ||= RequestLogger.new
       end
 
-      def loggers
-        @loggers ||= []
-      end
+      def_delegator :request_logger, :log
+      protected :log
     end
   end
 end
---
+++ lib/cequel/metal/request_logger.rb
@@ -0,0 +1,56 @@
+# -*- encoding : utf-8 -*-
+module Cequel
+  module Metal
+    #
+    # The Logger class encapsulates logging functionality for {Keyspace}.
+    #
+    # @api private
+    #
+    class RequestLogger
+      extend Forwardable
+      # @return [::Logger] An instance of Logger that responds to methods for
+      #   standard severity levels
+      attr_accessor :logger
+      # @return [Integer] Only log queries that take longer than threshold ms
+      attr_accessor :slowlog_threshold
+
+      def initialize
+        self.slowlog_threshold = 2000
+      end
+
+      #
+      # Log a CQL statement
+      #
+      # @param label [String] a logical label for this statement
+      # @param statement [String] the CQL statement to log
+      # @param bind_vars bind variables for the CQL statement
+      # @return [void]
+      #
+      def log(label, statement, *bind_vars)
+        return yield if logger.nil?
+
+        response = nil
+        begin
+          time = Benchmark.ms { response = yield }
+          severity = time >= slowlog_threshold ? :warn : :debug
+          logger.__send__(severity) do
+            format_for_log(label, "#{time.round.to_i}ms", statement, bind_vars)
+          end
+        rescue Exception => e
+          logger.error { format_for_log(label, 'ERROR', statement, bind_vars) }
+          raise
+        end
+        response
+      end
+
+      private
+
+      def format_for_log(label, timing, statement, bind_vars)
+        format('%s (%s) %s', label, timing, sanitize(statement, bind_vars))
+      end
+
+      def_delegator 'Cequel::Metal::Keyspace', :sanitize
+      private :sanitize
+    end
+  end
+end
