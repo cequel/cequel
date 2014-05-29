f2ed4a7174d6851889c136d87a22da57cc4a19c4
Mat Brown <mat.a.brown@gmail.com>
2014-05-28 22:33:43 -0400

    Extract created_at from auto-generated timeuuid if present

--- lib/cequel/record/timestamps.rb
+++ lib/cequel/record/timestamps.rb
@@ -5,12 +5,28 @@ module Cequel
       extend ActiveSupport::Concern
 
       module ClassMethods
+        def key(name, type, options = {})
+          super
+          if type == :timeuuid && options[:auto]
+            module_eval(<<-RUBY, __FILE__, __LINE__+1)
+              def created_at
+                read_attribute(#{name.inspect}).try(:to_time)
+              end
+            RUBY
+          end
+        end
+
         def timestamps
-          column :created_at, :timestamp
           column :updated_at, :timestamp
 
-          before_create :set_created_and_updated_at
-          before_update :set_updated_at
+          if method_defined?(:created_at)
+            before_save :set_updated_at
+          else
+            column :created_at, :timestamp
+
+            before_create :set_created_and_updated_at
+            before_update :set_updated_at
+          end
         end
       end
 
