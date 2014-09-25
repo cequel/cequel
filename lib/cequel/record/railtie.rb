08f3587c39622c54e446d32a1c7c3b22ad50ca10
Brian Miller <brian.miller@reachlocal.com>
2014-09-24 17:11:25 -0700

    Add ability to skip configuration step in railtie
    I did this to allow for the configuration of cequel using an initializer with
    ENV variables

--- lib/cequel/record/railtie.rb
+++ lib/cequel/record/railtie.rb
@@ -2,25 +2,28 @@ module Cequel
   module Record
     class Railtie < Rails::Railtie
       config.cequel = Record
+      config.cequel_skip_configuration = false
 
       def self.app_name
         Rails.application.railtie_name.sub(/_application$/, '')
       end
 
       initializer "cequel.configure_rails" do
-        config_path = Rails.root.join('config/cequel.yml').to_s
+        unless config.cequel_skip_configuration
+          config_path = Rails.root.join('config/cequel.yml').to_s
 
-        if File.exist?(config_path)
-          config = YAML::load(ERB.new(IO.read(config_path)).result)[Rails.env].
-            deep_symbolize_keys
-        else
-          config = {host: '127.0.0.1:9160'}
+          if File.exist?(config_path)
+            cequel_config = YAML::load(ERB.new(IO.read(config_path)).result)[Rails.env].
+              deep_symbolize_keys
+          else
+            cequel_config = {host: '127.0.0.1:9160'}
+          end
+          cequel_config.reverse_merge!(keyspace: "#{Railtie.app_name}_#{Rails.env}")
+          connection = Cequel.connect(cequel_config)
+
+          connection.logger = Rails.logger
+          Record.connection = connection
         end
-        config.reverse_merge!(keyspace: "#{Railtie.app_name}_#{Rails.env}")
-        connection = Cequel.connect(config)
-
-        connection.logger = Rails.logger
-        Record.connection = connection
       end
 
       rake_tasks do
