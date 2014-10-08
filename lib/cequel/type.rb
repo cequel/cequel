99941b3fb548a2e7abc4ccf8f815cc6873f6ca6e
Mat Brown <mat.a.brown@gmail.com>
2014-10-07 22:30:31 -0400

    Cast DateTime to UTC to ensure Rails 3 can convert it to a Time

--- lib/cequel/type.rb
+++ lib/cequel/type.rb
@@ -87,7 +87,7 @@ module Cequel
       when Time, ActiveSupport::TimeWithZone
         (value.to_r * 1000).round.to_s
       when DateTime
-        quote(value.to_time)
+        quote(value.utc.to_time)
       when Date
         quote(Time.gm(value.year, value.month, value.day))
       when Numeric, true, false, Cql::Uuid
