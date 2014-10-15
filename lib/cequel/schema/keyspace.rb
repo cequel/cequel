786aa586c0a2ef5db964c9ceaa7de56eeed9ba99
Lucas Mundim <lucas.mundim@gmail.com>
2014-10-15 17:49:42 -0300

    Allow config/cequel.yml to specify replication and durable_writes settings

--- lib/cequel/schema/keyspace.rb
+++ lib/cequel/schema/keyspace.rb
@@ -25,10 +25,10 @@ module Cequel
       # Create this keyspace in the database
       #
       # @param options [Options] persistence options for this keyspace.
-      # @option options [String] :class ("SimpleStrategy") the replication
-      #   strategy to use for this keyspace
-      # @option options [Integer] :replication_factor (1) the number of
-      #   replicas that should exist for each piece of data
+      # @option options [Hash] :replication ({ class: "SimpleStrategy",
+      #   replication_factor: 1 }) replication options for this keyspace
+      # @option options [Boolean] :durable_writes (true) durable_writes
+      #   option for the keyspace
       # @return [void]
       #
       # @see
@@ -40,17 +40,32 @@ module Cequel
           Metal::Keyspace.new(keyspace.configuration.except(:keyspace))
 
         options = options.symbolize_keys
-        options[:class] ||= 'SimpleStrategy'
-        if options[:class] == 'SimpleStrategy'
-          options[:replication_factor] ||= 1
-        end
-        options_strs = options.map do |name, value|
+
+        default_replication_options = {
+          class: "SimpleStrategy",
+          replication_factor: 1
+        }
+
+        replication_options = options[:replication] ||
+                              keyspace.configuration[:replication] ||
+                              default_replication_options
+
+        replication_options_strs = replication_options.map do |name, value|
           "'#{name}': #{Cequel::Type.quote(value)}"
         end
 
-        bare_connection.execute(<<-CQL)
+        if options.has_key? :durable_writes
+          durable_writes = options[:durable_writes]
+        elsif keyspace.configuration.has_key? :durable_writes
+          durable_writes = keyspace.configuration[:durable_writes]
+        else
+          durable_writes = true
+        end
+
+        bare_connection.execute(<<-CQL.strip_heredoc)
           CREATE KEYSPACE #{keyspace.name}
-          WITH REPLICATION = {#{options_strs.join(', ')}}
+          WITH REPLICATION = {#{replication_options_strs.join(', ')}}
+          AND durable_writes = #{durable_writes}
         CQL
       end
 
---
+++ spec/examples/schema/keyspace_spec.rb
@@ -0,0 +1,110 @@
+# -*- encoding : utf-8 -*-
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe Cequel::Schema::Keyspace do
+  let(:keyspace) { cequel.schema }
+
+  describe 'creating keyspace' do
+    before do
+      cequel.schema.drop!
+    end
+
+    after do
+      cequel.clear_active_connections!
+    end
+
+    let(:basic_config) {
+      {
+        host: Cequel::SpecSupport::Helpers.host,
+        port: Cequel::SpecSupport::Helpers.port,
+        keyspace: 'totallymadeup'
+      }
+    }
+
+    let(:schema_config) {
+      cequel.client.use('system')
+      cequel.client.execute("SELECT * FROM schema_keyspaces WHERE keyspace_name = 'totallymadeup'").first
+    }
+
+    context 'with default options' do
+      let(:config) { basic_config }
+
+      it 'uses default keyspace configuration' do
+        cequel.configure(config)
+        keyspace.create!
+        expect(schema_config).to eq({
+          "keyspace_name"=>"totallymadeup",
+          "durable_writes"=>true,
+          "strategy_class"=>"org.apache.cassandra.locator.SimpleStrategy",
+          "strategy_options"=>"{\"replication_factor\":\"1\"}"
+        })
+      end
+    end
+
+    context 'with explicit options' do
+      let(:config) { basic_config }
+
+      it 'uses specified options' do
+        cequel.configure(config)
+        keyspace.create! replication: { class: "SimpleStrategy", replication_factor: 2 }
+        expect(schema_config).to eq({
+          "keyspace_name"=>"totallymadeup",
+          "durable_writes"=>true,
+          "strategy_class"=>"org.apache.cassandra.locator.SimpleStrategy",
+          "strategy_options"=>"{\"replication_factor\":\"2\"}"
+        })
+      end
+    end
+
+    context 'with custom replication options' do
+      let(:config) {
+        basic_config.merge(replication: { class: "SimpleStrategy", replication_factor: 3 })
+      }
+
+      it 'uses default keyspace configuration' do
+        cequel.configure(config)
+        keyspace.create!
+        expect(schema_config).to eq({
+          "keyspace_name"=>"totallymadeup",
+          "durable_writes"=>true,
+          "strategy_class"=>"org.apache.cassandra.locator.SimpleStrategy",
+          "strategy_options"=>"{\"replication_factor\":\"3\"}"
+        })
+      end
+    end
+
+    context 'with another custom replication options' do
+      let(:config) {
+        basic_config.merge(replication: { class: "NetworkTopologyStrategy", datacenter1: 3, datacenter2: 2 })
+      }
+
+      it 'uses default keyspace configuration' do
+        cequel.configure(config)
+        keyspace.create!
+        expect(schema_config).to eq({
+          "keyspace_name"=>"totallymadeup",
+          "durable_writes"=>true,
+          "strategy_class"=>"org.apache.cassandra.locator.NetworkTopologyStrategy",
+          "strategy_options"=>"{\"datacenter1\":\"3\",\"datacenter2\":\"2\"}"
+        })
+      end
+    end
+
+    context 'with custom durable_write option' do
+      let(:config) {
+        basic_config.merge(durable_writes: false)
+      }
+
+      it 'uses default keyspace configuration' do
+        cequel.configure(config)
+        keyspace.create!
+        expect(schema_config).to eq({
+          "keyspace_name"=>"totallymadeup",
+          "durable_writes"=>false,
+          "strategy_class"=>"org.apache.cassandra.locator.SimpleStrategy",
+          "strategy_options"=>"{\"replication_factor\":\"1\"}"
+        })
+      end
+    end
+  end # describe 'creating keyspace'
+end
